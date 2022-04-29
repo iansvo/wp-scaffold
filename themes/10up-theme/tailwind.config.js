66a42b7f92e4f235e51f0f45f973b78ec20d218b
Ian Svoboda <ian@iansvoboda.com>
2022-04-29 09:26:30 -0400

    Updated tailwind content pattern, added standard watch
    Added watch task without hot reloading so hot reload is opt in only.

--- themes/10up-theme/tailwind.config.js
+++ themes/10up-theme/tailwind.config.js
@@ -4,7 +4,12 @@ const lineClamp = require('@tailwindcss/line-clamp');
 const aspectRatio = require('@tailwindcss/aspect-ratio');
 
 module.exports = {
-	content: ['assets/**/*.js', '**/*.php'],
+	content: [
+		'assets/**/*.js',
+		'**/*.php',
+		'includes/blocks/**/*.{js,php}',
+		'includes/block-*/**/*.{js,php}',
+	],
 	theme: {
 		extend: {},
 	},
