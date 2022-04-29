5752729e5187b11e515440d7979d6fb1cf785405
Ian Svoboda <ian@iansvoboda.com>
2022-04-29 11:49:46 -0400

    Setup pattern for ingesting theme.json colors into tailwind config

--- themes/10up-theme/tailwind.config.js
+++ themes/10up-theme/tailwind.config.js
@@ -2,9 +2,31 @@ const typography = require('@tailwindcss/typography');
 const forms = require('@tailwindcss/forms');
 const lineClamp = require('@tailwindcss/line-clamp');
 const aspectRatio = require('@tailwindcss/aspect-ratio');
+const themeJSON = require('./theme.json');
+
+function getThemePalette() {
+	const { palette } = themeJSON.settings.color;
+
+	return Array.isArray(palette) ? palette : [];
+}
+
+function getThemeColors() {
+	const palette = getThemePalette();
+
+	if (!palette) return [];
+
+	const colors = {};
+
+	palette.forEach((paletteColor) => {
+		const { slug, color } = paletteColor;
+
+		colors[slug] = color;
+	});
+
+	return colors;
+}
 
 module.exports = {
-	content: [
 		'assets/**/*.js',
 		'**/*.php',
 		'includes/**/*.js',
@@ -12,7 +34,11 @@ module.exports = {
 		'includes/block-*/**/*.php',
 	],
 	theme: {
-		extend: {},
+		extend: {
+			colors: {
+				...getThemeColors(),
+			},
+		},
 	},
 	plugins: [typography, forms, lineClamp, aspectRatio],
 };
